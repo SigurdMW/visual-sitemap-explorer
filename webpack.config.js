8cacaa2869bac5fc45241df3b839e9e4b00160b6
Sigurd Wahl <sigurd.wahl@knowit.no>
2017-06-22 10:07:59 +0200

    setup webpack and started prosject

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+             loader: 'babel-loader',
+             options: {
+               presets: ['es2015']
+             }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: extractPlugin.extract({
+          use: ['css-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+     title: 'Output Management',
+     template: 'static/index.html',
+   })
+  ]
+}
